8372e8c2c74575a41895092f805644495fba49ce
Danny Eisenga <d.eisenga@gmail.com>
2024-04-04 08:58:51 +0200

    Add toSchema util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
 import * as S from '@effect/schema/Schema';
+import * as Reader from './Reader';
 import {FieldType} from './types';
+import * as Writer from './Writer';
 
 export const pipeFieldType = <A, B>(
   fieldType: FieldType<A>,
@@ -11,3 +13,17 @@ export const pipeFieldType = <A, B>(
   write: (writer, value) => fieldType.write(writer, preWrite(value)),
   schema: schema,
 });
+
+export const toSchema = <A>(fieldType: FieldType<A>): S.Schema<Uint8Array, A> => S.transform(
+  S.instanceOf(Uint8Array),
+  fieldType.schema,
+  bytes => {
+    const reader = Reader.create(bytes);
+    return fieldType.read(reader);
+  },
+  value => {
+    const writer = Writer.create();
+    fieldType.write(writer, value);
+    return Writer.finish(writer);
+  }
+);
